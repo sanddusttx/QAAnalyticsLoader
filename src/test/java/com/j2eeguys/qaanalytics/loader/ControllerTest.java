be922d88e7cd9ee95d4eda4e731181ea58378c28
Gorky <gorky@j2eeguys.com>
2019-04-06 17:24:40 -0500

    Adding JUnit support to gradle.
    General JUnit support.
    Tagged UI tests.

--- src/test/java/com/j2eeguys/qaanalytics/loader/ControllerTest.java
+++ src/test/java/com/j2eeguys/qaanalytics/loader/ControllerTest.java
@@ -3,12 +3,13 @@
  */
 package com.j2eeguys.qaanalytics.loader;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 /**
- * @author honor
+ * @author sanddust@j2eeguys.com
  *
  */
 class ControllerTest {
@@ -17,6 +18,7 @@ class ControllerTest {
 	 * Test method for {@link com.j2eeguys.qaanalytics.loader.Controller#queryDate()}.
 	 */
 	@Test
+	@Tag("UI")
 	void testQueryDate() {
 		Controller c = new Controller();
 		c.queryDate();
